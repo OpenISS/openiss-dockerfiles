eac13f7793ee942f77d0accf702390989b62ab53
Jonathan Llewellyn <inexistenz@gmail.com>
2022-08-18 17:59:25 -0400

    finished glfwTest.cpp

--- OpenGL/GLFWtest/glfw_test.cpp
+++ OpenGL/GLFWtest/glfw_test.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <list>
+#include <map>
 
 #define GLEW_STATIC 1
 #include <GL/glew.h>
@@ -8,16 +8,36 @@
 
 using namespace std;
 
+map<int, const char*> glfwErrors {
+    { GLFW_NO_ERROR           , "GLFW_NO_ERROR" },
+    { GLFW_NOT_INITIALIZED    , "GLFW_NOT_INITIALIZED" },
+    { GLFW_NO_CURRENT_CONTEXT , "GLFW_NO_CURRENT_CONTEXT" },
+    { GLFW_INVALID_ENUM       , "GLFW_INVALID_ENUM" },
+    { GLFW_INVALID_VALUE      , "GLFW_INVALID_VALUE" },
+    { GLFW_OUT_OF_MEMORY      , "GLFW_OUT_OF_MEMORY" },
+    { GLFW_API_UNAVAILABLE    , "GLFW_API_UNAVAILABLE" },
+    { GLFW_VERSION_UNAVAILABLE, "GLFW_VERSION_UNAVAILABLE" },
+    { GLFW_PLATFORM_ERROR     , "GLFW_PLATFORM_ERROR" },
+    { GLFW_FORMAT_UNAVAILABLE , "GLFW_FORMAT_UNAVAILABLE" },
+    { GLFW_NO_WINDOW_CONTEXT  , "GLFW_NO_WINDOW_CONTEXT" }
+};
+
+void error_cb(int error_code, const char* description)
+{
+    cerr << "Error: " << glfwErrors[error_code] << " " << description << endl;
+}
+
 int main(int argc, char*argv[])
 {
-    // Initialize GLFW and OpenGL version
+    glfwSetErrorCallback(error_cb);
     glfwInit();
     
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    // Create Window and rendering context using GLFW, resolution is 800x600
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Comp371 - Lab 03", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW Test - Press ESC to Quit", NULL, NULL);
     if (window == NULL)
     {
         std::cerr << "Failed to create GLFW window" << std::endl;
@@ -25,6 +45,15 @@ int main(int argc, char*argv[])
         return -1;
     }
     glfwMakeContextCurrent(window);
+
+    glewExperimental = true;
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW: " << glewGetErrorString(err) << std::endl;
+
+        glfwTerminate();
+        return -1;
+    }
 
     while(!glfwWindowShouldClose(window))
     {
@@ -38,7 +67,6 @@ int main(int argc, char*argv[])
 
     }
 
-    // Shutdown GLFW
     glfwTerminate();
     
 	return 0;
